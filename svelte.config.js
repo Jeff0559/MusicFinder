309d2c5b094b59fe50ed26f8c33465e271f6578e
Jeff0559 <stettjer@students.zhaw.ch>
2025-12-02 21:44:21 +0100

    Fix: correct svelte.config.js for Netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter()
+    }
 };
 
 export default config;
